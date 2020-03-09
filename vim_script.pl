400c705a02a3396a72d4cace714c5cb70043fec4
Radi Radev <33155474+rrraddy@users.noreply.github.com>
2020-03-09 11:41:48 +0200

    Update vim_script.pl

--- vim_script.pl
+++ vim_script.pl
@@ -1,12 +1,33 @@
 #!/usr/bin/perl
 
 my $rc_file = '/home/raddy/.vimrc';
-my $conf =q(
+my $vimrc =q(
+colorscheme cobalt
+
+map<F2> :NERDTree
+autocmd BufWinEnter *.py nnoremap <F8> :w<CR>:!python3 %:p<cr>
+map<F9> :w !perl -I/usr/local/www/lib/perl -cT <cr>
+map<F7> :w !node
+map<F5> :set nu! 
+
+execute pathogen#infect()
+syntax on
+filetype plugin indent on
+autocmd BufEnter * let &titlestring = ' ' . expand("%:t")
+    
+set title
+set nomodeline
+set nobackup
+set noundofile
+set mouse=
+set number " <- shows line numbers
+set encoding=utf8
+set keywordprg=perldoc\-f
 ); 
 
 open(FH, '>', $rc_file) or die $!;
 
-print FH $conf;
+print FH $vimrc;
 
 close FH;
 
