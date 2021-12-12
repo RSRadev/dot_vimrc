87a565923ccb404021fb37f1bab8966654ac68b2
Radi Radev <33155474+raddysr@users.noreply.github.com>
2021-12-12 15:21:22 +0200

    Create code_ext.sh

---
+++ code_ext.sh
@@ -0,0 +1,12 @@
+code --install-extension GitHub.github-vscode-theme
+code --install-extension golang.go
+code --install-extension humao.rest-client
+code --install-extension in4margaret.compareit
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension RandomFractalsInc.vscode-data-preview
+code --install-extension vscode-icons-team.vscode-icons
