99636ad8b34f8b3be58146982bfed4af61fe6bc6
radi <radi@pop-os.localdomain>
2023-12-03 12:35:46 +0200

    tabs

--- set_vim.pl
+++ set_vim.pl
@@ -25,7 +25,12 @@ syntax on
 filetype plugin indent on
 
 autocmd BufEnter * let &titlestring = ' ' . expand("%:t")
-    
+
+
+
+set tabstop=4
+set shiftwidth=4
+set expandtab
 set title
 set nomodeline
 set nobackup
